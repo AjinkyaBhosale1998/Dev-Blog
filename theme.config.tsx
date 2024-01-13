9d84da29da39c7c3f876f1d0c27a6250ea2cb0b9
Ajinkya Bhosale <ajinkya.bhosale@ascendion.com>
2024-01-13 15:21:08 +0530

    commit nextra blog project

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Pixels with Purpose</span>,
+  project: {
+    link: 'https://github.com/AjinkyaBhosale1998',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: ['Made with Nextra Docs Template'],
+  },
+}
+
+export default config
